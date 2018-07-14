b40bcb9e121a2f04f1df977cb38e17c5df1a2579
Avinash <avinash.mait@gmail.com>
2018-07-14 17:22:37 +0530

    1st Step - 1st failing test for scanning an apple

--- src/test/scala/cart/CheckoutSpec.scala
+++ src/test/scala/cart/CheckoutSpec.scala
@@ -6,7 +6,16 @@ class CheckoutSpec extends FlatSpec with Matchers {
 
   "Checkout" should "return total as zero when nothing is scanned" in {
     val expectedTotal = Money(0)
+
     val checkout = new Checkout()
+    checkout.total() should be(expectedTotal)
+  }
+
+  it should "scan an apple and return total as 0.60" in {
+    val expectedTotal = Money(0.60)
+
+    val checkout = new Checkout()
+    checkout.scan("APPLE")
     checkout.total() should be(expectedTotal)
   }
 
