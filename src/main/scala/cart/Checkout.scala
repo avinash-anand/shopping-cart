8d28f67ba5014a558c9808dad2dc71bbe0d5892a
Avinash <avinash.mait@gmail.com>
2018-07-14 18:24:26 +0530

    2nd Step - passing test when applying offer of apples on cart with apples and oranges

--- src/main/scala/cart/Checkout.scala
+++ src/main/scala/cart/Checkout.scala
@@ -4,17 +4,20 @@ import scala.collection.mutable
 
 class Checkout() {
 
-  private var totalCost: Money = Money(0)
   private val cartItems = mutable.ArrayBuffer[Item]()
-  private var discount = Money(0)
+  private var offer: Option[BuyOneAppleGetOneFree] = None
 
   def scan(items: Item*): Unit = cartItems.append(items: _*)
 
-  def applyOffer(offer: BuyOneAppleGetOneFree): Unit = discount = Money(new Apple().price.value * (cartItems.size / 2))
+  def applyOffer(offer: BuyOneAppleGetOneFree): Unit = this.offer = Some(offer)
 
   def total(): Money = {
-    totalCost = cartItems.foldLeft(Money(0))((total, item) => total + item.price)
-    totalCost - discount
+    val  totalCost = cartItems.foldLeft(Money(0))((total, item) => total + item.price)
+    val appleCount = cartItems.collect {case a: Apple => a}.size
+    val appleDiscount = offer.fold(Money(0)){_ =>
+      val discountedApples: Int = appleCount / 2
+      Money(discountedApples * new Apple().price.value)}
+    totalCost - appleDiscount
   }
 
 }
